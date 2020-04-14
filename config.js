b80ec7cd9228a820c1e7b2791cd007ef41d9c34a
Vigneshwaran Balamurugan <58186438+vbj187@users.noreply.github.com>
2020-04-14 14:51:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'Bf7fhNhtOkNYwz9d1MjNE0iMA',
-    consumer_secret: 'yF0H5VPZuKDc1gLa7BHZdTrue7sQOVLb3VBlYGOoj5Cdekb5wc',
-    access_token: '1248537328148045825-NGcrO2ZREe3QK0DI2EN4KBHt1uPHcx',
-    access_token_secret: 'SQ5gkdZILbfX5edAry0ptP6HrM9qGnO06msnF3UTaq7J5'
+    consumer_key: '#########################',
+    consumer_secret: '##################################################',
+    access_token: '##################################################',
+    access_token_secret: '#############################################'
 }
